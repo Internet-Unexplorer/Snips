7ed914ed41767a3d25add604f9b9730243f1a178
tej273 <33614803+tej273@users.noreply.github.com>
2017-12-04 20:29:21 +0530

    Create ajaxSnip.js

---
+++ JS/ajaxFormHandling/ajaxSnip.js
@@ -0,0 +1,32 @@
+function fName(id){
+
+	if(id == ""){  // Validation
+		alert("Err-msg");
+	} else {
+
+      var conf = confirm("Are you Sure? Click OK to confirm."); // Action confirmation
+			if (conf != true) {
+				return false;
+			}
+
+		_("addtocart"+id).innerHTML = 'Please wait ...';
+        _("addtocart"+id).disabled = true;
+        
+        
+		var ajax = ajaxObj("POST", "parser/php_parser.php"); // ajax starts
+        ajax.onreadystatechange = function() {
+	        if(ajaxReturn(ajax) == true) {
+	            if(ajax.responseText == "doesntexists"){
+					alert("You do not have this item in your cart anymore.")
+				}
+                else if(ajax.responseText == "notLoggedIn") {
+					$("#myModal").modal();
+				}
+                else if(ajax.responseText == "removed") {
+                    location.reload();
+				}
+	        }
+        }
+        ajax.send("removeFromCart=1&id="+id); // ajax ends
+	}
+}
